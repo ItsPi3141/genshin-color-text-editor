8e8cb78a4f4281660b5306e569d687a8a58b32a0
Pi <piisthebestthingintheuniverse@gmail.com>
2024-05-15 23:16:50 -0700

    add copy button

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,27 @@ import { useState } from "react";
 
 export default function Home() {
 	const [result, setResult] = useState("");
+	const [copied, setCopied] = useState(false);
 	return (
 		<main className="flex flex-col justify-between items-center gap-8 bg-darkest p-4 sm:p-12 md:p-24 pb-8 min-h-screen text-light">
 			<div className="flex flex-col gap-4 w-full max-w-[800px]">
 				<h1 className="mb-4 sm:mb-8 text-2xl sm:text-3xl">Genshin Color Text Editor</h1>
 				<p>Edit</p>
 				<TextEditor onChange={(value) => setResult(value)} />
-				<p className="mt-8">Result</p>
+				<div className="flex justify-between mt-8">
+					<p>Result</p>
+					<button
+						type="button"
+						className={`border-accent ${copied ? "bg-light text-dark" : "bg-dark"} px-2 py-1 border border-opacity-10 rounded`}
+						onClick={() => {
+							navigator.clipboard.writeText(result);
+							setCopied(true);
+							setTimeout(() => setCopied(false), 2000);
+						}}
+					>
+						{copied ? "Copied" : "Copy"}
+					</button>
+				</div>
 				<div className="border-2 border-accent bg-darker p-4 border-opacity-50 rounded w-full min-h-[3.75em]">
 					{result.split("\n").map((line) => (
 						<p key={null} className="[word-break:break-word] [overflow-wrap:anywhere]">
