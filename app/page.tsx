4d63aa9625e1b4de3de9d37a3e42dbd5814beeb2
Pi <piisthebestthingintheuniverse@gmail.com>
2024-05-15 23:14:18 -0700

    responsive layout for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,17 @@ import { useState } from "react";
 export default function Home() {
 	const [result, setResult] = useState("");
 	return (
-		<main className="flex flex-col justify-between items-center gap-8 bg-darkest p-24 pb-8 min-h-screen text-light">
+		<main className="flex flex-col justify-between items-center gap-8 bg-darkest p-4 sm:p-12 md:p-24 pb-8 min-h-screen text-light">
 			<div className="flex flex-col gap-4 w-full max-w-[800px]">
-				<h1 className="mb-8 text-3xl">Genshin Color Text Editor</h1>
+				<h1 className="mb-4 sm:mb-8 text-2xl sm:text-3xl">Genshin Color Text Editor</h1>
 				<p>Edit</p>
 				<TextEditor onChange={(value) => setResult(value)} />
 				<p className="mt-8">Result</p>
 				<div className="border-2 border-accent bg-darker p-4 border-opacity-50 rounded w-full min-h-[3.75em]">
 					{result.split("\n").map((line) => (
-						<p key={null}>{line}</p>
+						<p key={null} className="[word-break:break-word] [overflow-wrap:anywhere]">
+							{line}
+						</p>
 					))}
 				</div>
 			</div>
